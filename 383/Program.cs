71208cbbe5d6ef8f2026597b0a5fb1a2760a41e0
Bruno de Avila <brunomunao@gmail.com>
2023-05-19 22:15:30 -0300

    More :)

---
+++ 383/Program.cs
@@ -0,0 +1,23 @@
+﻿bool CanConstruct(string ransomNote, string magazine)
+{
+	Dictionary<char, int> letterCount = new Dictionary<char, int>();
+	foreach (var letter in magazine)
+	{
+		if (letterCount.ContainsKey(letter))
+			letterCount[letter]++;
+		else
+			letterCount[letter] = 1;
+	}
+	foreach (var letter in ransomNote)
+	{
+		if (letterCount.ContainsKey(letter) && letterCount[letter] > 0)
+			letterCount[letter]--;
+		else
+			return false;
+	}
+
+	return true;
+}
+
+Console.WriteLine(CanConstruct("a", "b"));
+
---
+++ 876/Program.cs
@@ -0,0 +1,37 @@
+﻿using _876;
+
+ListNode MiddleNode(ListNode head)
+{
+	ListNode slow = head;
+	ListNode fast = head;
+
+	while (fast != null && fast.next != null)
+	{
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+	return slow;
+}
+
+void PrintList(ListNode head)
+{
+	ListNode current = head;
+	while (current != null)
+	{
+		Console.WriteLine(current.val);
+		current = current.next;
+	}
+}
+
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+head.next.next.next.next.next = new ListNode(6);
+
+var list = MiddleNode(head);
+
+
+PrintList(list);
+
